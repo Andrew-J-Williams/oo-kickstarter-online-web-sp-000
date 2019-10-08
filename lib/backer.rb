d134ae25d2ecc96657d7ec1b2aad6c973532209d
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 00:19:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
     @backed_projects = [] # We also initialize our 'backed_projects' by setting it equal to an empty array.
   end
 
-  def back_project(project_name)
+  def back_project(project_name) # This method uses the argument of a 'Project' instance and pushes it into our 'backed_projects' array.
     @backed_projects << project_name
   end
 
