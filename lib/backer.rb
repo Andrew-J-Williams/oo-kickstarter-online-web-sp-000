018111f158cff6a6cd18acb56582f57303d0a505
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 00:29:35 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,6 +9,7 @@ class Backer
 
   def back_project(project_name) # This method uses the argument of a 'Project' instance and pushes it into our 'backed_projects' array.
     @backed_projects << project_name
+    project_name.backers << 
   end
 
 end
