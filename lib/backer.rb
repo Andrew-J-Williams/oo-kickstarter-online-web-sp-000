127fe8c0b2c5b42fe29d68540d1001f80a3fdf9b
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 00:09:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
 
   attr_reader :name, :backed_projects # The 'name' and 'backed_projects' are accessible through the reader.
 
-  def initialize(name)
+  def initialize(name) # We initialize 'Backer' with a name.
     @name = name
     @backed_projects = []
   end
