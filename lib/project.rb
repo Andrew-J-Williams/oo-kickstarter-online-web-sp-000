49ade2934bdbc409fcafa8fa0db59bac56bb7b4f
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 00:12:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,7 +2,7 @@ class Project
 
   attr_reader :title, :backers # Again, these attributes are accessible through the reader.
 
-  def initialize(title) # We initialize with a name and set our backers equal to an empty array.
+  def initialize(title) # We initialize with a 'title' (NOT a name) and set our backers equal to an empty array.
     @title = title
     @backers = []
   end
